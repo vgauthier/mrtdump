587263cb92b416b254814f9bc8f899cf5c08080b
Vincent Gauthier <pub@luxbulb.org>
2025-07-31 11:55:07 +0200

    add test for mrtmessage

---
+++ internal/message/mrtmessage_test.go
@@ -0,0 +1,15 @@
+package message
+
+import (
+	"testing"
+)
+
+func TestMRTMessage(t *testing.T) {
+	t.Run("Test MRTMessage Read", func(t *testing.T) {
+		// Add your test cases here
+	})
+
+	t.Run("Test MRTMessage String", func(t *testing.T) {
+		// Add your test cases here
+	})
+}
