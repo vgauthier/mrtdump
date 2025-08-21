9f792172d2c9aef4f67e85797cfe9cbd80f9da97
Vincent Gauthier <pub@luxbulb.org>
2025-08-21 16:12:31 +0200

    add new test case

--- src/mrt/message/rib_entry.rs
+++ src/mrt/message/rib_entry.rs
@@ -41,6 +41,7 @@ impl RibEntry {
             DateTime::from_timestamp(originated_time.into(), 0).ok_or(Error::BadMrtHeader)?;
         let attribute_length = reader.read_u16::<BigEndian>()?;
 
+        // Validate peer index
         if peer_index as usize >= peer_index_table.entries.len() {
             return Err(Error::InvalidPeerIndex(peer_index));
         }
@@ -111,6 +112,7 @@ mod tests {
     use crate::mrt::message::bgp_attribute::{BgpOrigin, BgpOriginType};
     use crate::mrt::message::peer_index_table::{PeerEntry, PeerIndexTable};
     use std::io::Cursor;
+    use std::net::Ipv4Addr;
 
     #[test]
     fn test_rib_entry_serialization() {
@@ -131,14 +133,18 @@ mod tests {
         let mut cursor = Cursor::new(vec![
             0x00, 0x00, // Peer index
             0x00, 0x00, 0x00, 0x01, // Originated time
-            0x00, 0x13, // attributes length
-            0x10, 0x01, 0x00, 0x01, // BGP Header type=1 (origin) length=1 4
-            0x00, // Origin IGP 1
-            0x10, 0x02, 0x00, 0x0a, // BGP Header type=2 (aspath) length=10 4
-            0x00, // AS Path Segment Type 1
-            0x02, // AS Path Segment Length 1
-            0x00, 0x00, 0x00, 0x01, // AS Path Segment 1 4
-            0x00, 0x00, 0x00, 0x02, // AS Path Segment 2 4
+            0x00, 0x1d, // attributes length 29
+            0x10, 0x01, 0x00, 0x01, // BGP Header type=1 (origin) length=1
+            0x00, // Origin IGP
+            0x10, 0x02, 0x00, 0x0a, // BGP Header type=2 (aspath) length=10
+            0x00, // AS Path Segment Type
+            0x02, // AS Path Segment Length
+            0x00, 0x00, 0x00, 0x01, // AS Path Segment 1
+            0x00, 0x00, 0x00, 0x02, // AS Path Segment 2
+            0x10, 0x23, 0x00, 0x01, // BGP Header type=35 (wrong) length=1
+            0x00, // No data
+            0x10, 0x03, 0x00, 0x04, // BGP Header type=3 (next_hop) length=4
+            0xC0, 0x00, 0x02, 0x01, // Next Hop IP
         ]);
 
         let rib_entry = RibEntry::from_reader(&mut cursor, &peer_index_table);
@@ -152,5 +158,10 @@ mod tests {
         let segments = rib_entry.bgp_as_path.unwrap().segments;
         let expected_segments = vec![1, 2];
         assert_eq!(segments, expected_segments);
+        let _expected_next_hop = BgpNextHop(Ipv4Addr::new(192, 0, 2, 1));
+        assert!(matches!(
+            rib_entry.bgp_next_hop.unwrap(),
+            _expected_next_hop
+        ));
     }
 }
